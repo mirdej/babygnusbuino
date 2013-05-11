7579da9cf581b03af0a5467d101dd11908642698
Michael Egger <me@anyma.ch>
2013-05-11 04:11:20 +0200

    added midi support

---
+++ cores/midibabygnusbuino/main.cpp
@@ -0,0 +1,265 @@
+#include <Arduino.h>
+#include <avr/wdt.h>
+#include "midignusb.h"
+#include "usbdrv.h"
+#include "../../../../libraries/MIDI/MIDI.h"	// not very clean, I know
+
+
+
+// ------------------------------------------------------------------------------
+// - Status Leds
+// ------------------------------------------------------------------------------
+// 							(on means  set to 0 as we sink the LEDs )
+ 
+ 
+
+unsigned long blinkstop;
+
+#if defined(__AVR_ATtiny85__)
+void statusLedOff(StatusLeds led) 		{}
+void statusLedOn(StatusLeds led) 		{}
+void statusLedToggle(StatusLeds led)	{}
+void statusLedBlink(StatusLeds led) {}
+
+#else
+void statusLedOff(StatusLeds led) 		{PORTD &= ~(1 << led); }
+void statusLedOn(StatusLeds led) 		{PORTD |= (1 << led);}
+void statusLedToggle(StatusLeds led)	{PORTD ^= 1 << led;}
+void statusLedBlink(StatusLeds led) {
+		statusLedOn(led);
+		blinkstop = millis()+50;
+}
+#endif
+
+// ------------------------------------------------------------------------------
+// - Start Bootloader
+// ------------------------------------------------------------------------------
+// dummy function doing the jump to bootloader section (Adress 1C00 on Atmega644)
+
+void (*jumpToBootloader)(void) = (void (*)(void))0xF800; __attribute__ ((unused))
+
+void startBootloader(void) {
+		
+		
+		MCUCSR &= ~(1 << PORF);			// clear power on reset flag
+										// this will hint the bootloader that it was forced
+	
+		cli();							// turn off interrupts
+		wdt_disable();					// disable watchdog timer
+		usbDeviceDisconnect(); 			// disconnect gnusb from USB bus
+		
+		ADCSRA &= ~(( 1 << ADIE) | ( 1 << ADEN));	// disable ADC interrupts
+													// disable ADC (turn off ADC power)
+
+		statusLedOff(StatusLed_Yellow);		
+		statusLedOff(StatusLed_Green);
+
+		jumpToBootloader();
+}
+
+
+
+
+
+
+// ------------------------------------------------------------------------------
+// - usbFunctionDescriptor
+// ------------------------------------------------------------------------------
+
+unsigned char usbFunctionDescriptor(usbRequest_t * rq)
+{
+
+	if (rq->wValue.bytes[1] == USBDESCR_DEVICE) {
+		usbMsgPtr = (unsigned char *) deviceDescrMIDI;
+		return sizeof(deviceDescrMIDI);
+	} else {		/* must be config descriptor */
+		usbMsgPtr = (unsigned char *) configDescrMIDI;
+		return sizeof(configDescrMIDI);
+	}
+}
+
+// ------------------------------------------------------------------------------
+// - usbFunctionSetup
+// ------------------------------------------------------------------------------
+// this function gets called when the usb driver receives a non standard request
+// that is: our own requests defined in ../common/gnusb_cmds.h
+unsigned char usbFunctionSetup(unsigned char data[8])
+{
+	void *pVoid = data;
+	usbRequest_t    *rq = static_cast<usbRequest_t*>(pVoid);
+
+	if ((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) {	/* class request type */
+
+		/*  Prepare bulk-in endpoint to respond to early termination   */
+		if ((rq->bmRequestType & USBRQ_DIR_MASK) ==
+		    USBRQ_DIR_HOST_TO_DEVICE) {}
+	}
+	switch (data[1]) {
+// 								----------------------------   Start Bootloader for reprogramming the gnusb    		
+		case GNUSBCORE_CMD_START_BOOTLOADER:
+
+			startBootloader();
+			break;
+				
+		default:
+			break;
+	}
+	return 0xff;
+}
+
+
+// ---------------------------------------------------------------------------
+//  usbFunctionRead                                                          
+// ---------------------------------------------------------------------------
+
+unsigned char usbFunctionRead(unsigned char * data, unsigned char len)
+{
+
+//	statusLedToggle(StatusLed_Yellow);
+
+//???? thats from http://cryptomys.de/horo/V-USB-MIDI/index.html
+	data[0] = 0;
+	data[1] = 0;
+	data[2] = 0;
+	data[3] = 0;
+	data[4] = 0;
+	data[5] = 0;
+	data[6] = 0;
+
+	return 7;
+}
+
+
+/*---------------------------------------------------------------------------*/
+/* usbFunctionWrite                                                          */
+/*---------------------------------------------------------------------------*/
+
+unsigned char usbFunctionWrite(unsigned char * data, unsigned char len)
+{
+	return 1;
+}
+
+/*---------------------------------------------------------------------------*/
+/* usbFunctionWriteOut                                                       */
+/*                                                                           */
+/* this Function is called if a MIDI Out message (from PC) arrives.          */
+/*                                                                           */
+/*---------------------------------------------------------------------------*/
+void usbFunctionWriteOut(unsigned char * data, unsigned char len)
+{
+
+	statusLedBlink(StatusLed_Yellow);
+
+	while (len >= sizeof(midi_msg)) {
+	
+		midi_msg* msg = (midi_msg*)data;
+		
+		MIDI.receiveMIDI(msg->byte[0],msg->byte[1],msg->byte[2]);
+
+		data += sizeof(midi_msg);
+		len -= sizeof(midi_msg);
+	}
+}
+
+
+
+// ------------------------------------------------------------------------------
+// - doPeriodical
+// ------------------------------------------------------------------------------
+// stuff that has do be done often in the loop and not forgotten while delaying
+
+
+void doPeriodical(void) {
+
+		if (blinkstop) {
+			if (millis() >= blinkstop) {
+				statusLedOff(StatusLed_Yellow);
+				blinkstop = 0;
+			}
+		}	
+		
+		usbPoll();
+		MIDI.sendMIDI();        
+
+
+		wdt_reset();
+}		
+
+
+
+
+// ------------------------------------------------------------------------------
+// --------------------- Init AD Converter
+// ------------------------------------------------------------------------------
+void adInit(void){
+
+
+
+	ADCSRA |= (1 << ADEN);				// enable ADC (turn on ADC power)
+	ADCSRA &= ~(1 << ADATE);			// default to single sample convert mode
+										// Set ADC-Prescaler (-> precision vs. speed)
+
+	ADCSRA = ((ADCSRA & ~ADC_PRESCALE_MASK) | ADC_PRESCALE_DIV64); // Set ADC Reference Voltage to AVCC
+	
+	#if defined(__AVR_ATtiny85__)
+		ADMUX = 0;	// make sure we don't have AREF on PB0 which is used as a usb pullup
+	#else			// for bigger chips, use AREF with capacitor
+		ADMUX |= (1 << REFS0);		
+		ADMUX &= ~(1 << REFS1);
+	#endif
+
+	ADCSRA &= ~(1 << ADLAR);				// set to right-adjusted result//	sbi(ADCSRA, ADIE);				// enable ADC interrupts
+	ADCSRA &= ~(1 << ADIE);				// disable ADC interrupts
+//	ad_initialized = 1;
+}
+
+
+// ------------------------------------------------------------------------------
+// - main
+// ------------------------------------------------------------------------------
+
+int main(void)
+{
+	MCUCSR = (1 << PORF);			// set power on reset flag just to be sure
+
+	init();
+	adInit();
+
+	wdt_enable(WDTO_1S);	// enable watchdog timer
+	
+#if defined(__AVR_ATtiny85__)
+	DDRB = 1; 	//PB0 is output
+#else
+	// set PORT D Directions -> 1110 0000, output 0 on unconnected PD7
+	DDRD = 0xe0; 	// 1110 0000 -> set PD0..PD4 to inputs -> USB pins
+	PORTD = 0x70; 	// set Pullup for Bootloader Jumper, no pullups on USB pins -> 0111 0000
+#endif
+
+	unsigned char   i = 0;
+
+    usbInit();
+  
+    // enforce USB re-enumerate: 
+
+	cli();    
+    usbDeviceDisconnect();  // do this while interrupts are disabled 
+    while(--i){         // fake USB disconnect for > 250 ms 
+        wdt_reset();
+        delay(1);
+    }
+
+    usbDeviceConnect();
+    sei();
+	
+	statusLedOn(StatusLed_Green);
+	
+	setup();
+
+	for (;;) {
+		loop();	
+		doPeriodical();
+	}
+        
+	return 0;
+}
+
---
+++ cores/midibabygnusbuino/wiring_analog.cpp
@@ -0,0 +1,149 @@
+/*
+  wiring_analog.c - analog input and output
+  Part of Arduino - http://www.arduino.cc/
+
+  Copyright (c) 2005-2006 David A. Mellis
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General
+  Public License along with this library; if not, write to the
+  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+  Boston, MA  02111-1307  USA
+
+  Modified 28 September 2010 by Mark Sproul
+
+  $Id: wiring.c 248 2007-02-03 15:36:30Z mellis $
+*/
+#include "Arduino.h"
+#include "wiring_private.h"
+#include "pins_arduino.h"
+
+
+uint8_t analog_reference = DEFAULT;
+unsigned char ad_initialized = 0;
+
+void analogReference(uint8_t mode)
+{
+	// can't actually set the register here because the default setting
+	// will connect AVCC and the AREF pin, which would cause a short if
+	// there's something connected to AREF.
+	analog_reference = mode;
+}
+
+
+
+
+// ------------------------------------------------------------------------------
+// --------------------- AnalogRead
+// ------------------------------------------------------------------------------
+
+int analogRead(uint8_t pin)
+{
+
+	uint8_t low, high;
+
+/*	if (!ad_initialized) {
+		adInit();
+	}*/
+ 	ADMUX = (ADMUX & ~ADC_MUX_MASK) | (pin & ADC_MUX_MASK);		// set channel (from midi_gnusb.c)
+
+	// start the conversion
+	ADCSRA |= (1 << ADIF);			// clear hardware "conversion complete" flag 
+	ADCSRA |= (1 << ADSC);			// start conversion
+
+	// ADSC is cleared when the conversion finishes
+	while (bit_is_set(ADCSRA, ADSC)){
+		doPeriodical();
+	};
+
+	// we have to read ADCL first; doing so locks both ADCL
+	// and ADCH until ADCH is read.  reading ADCL second would
+	// cause the results of each conversion to be discarded,
+	// as ADCL and ADCH would be locked when it completed.
+	 	return (ADCL | ADCH << 8);
+
+	low  = ADCL;
+	high = ADCH;
+
+	// combine the two bytes
+	return (high << 8) | low;
+}
+
+// Right now, PWM output only works on the pins with
+// hardware support.  These are defined in the appropriate
+// pins_*.c file.  For the rest of the pins, we default
+// to digital output.
+void analogWrite(uint8_t pin, int val)
+{
+	// We need to make sure the PWM output is enabled for those pins
+	// that support it, as we turn it off when digitally reading or
+	// writing with them.  Also, make sure the pin is in output mode
+	// for consistenty with Wiring, which doesn't require a pinMode
+	// call for the analog output pins.
+	pinMode(pin, OUTPUT);
+	if (val == 0)
+	{
+		digitalWrite(pin, LOW);
+	}
+	else if (val == 255)
+	{
+		digitalWrite(pin, HIGH);
+	}
+	else
+	{
+		switch(digitalPinToTimer(pin))
+		{
+			// XXX fix needed for atmega8
+			#if defined(TCCR0) && defined(COM00) && !defined(__AVR_ATmega8__)
+			case TIMER0A:
+				// connect pwm to pin on timer 0
+				sbi(TCCR0, COM00);
+				OCR0 = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR0A) && defined(COM0A1)
+			case TIMER0A:
+				// connect pwm to pin on timer 0, channel A
+				sbi(TCCR0A, COM0A1);
+				OCR0A = val; // set pwm duty
+				break;
+			#endif
+
+			#if defined(TCCR0A) && defined(COM0B1)
+			case TIMER0B:
+				// connect pwm to pin on timer 0, channel B
+				sbi(TCCR0A, COM0B1);
+				OCR0B = val; // set pwm duty
+				break;
+			#endif
+
+
+			case TIMER1B:
+				// connect pwm to pin on timer 1, channel B
+				sbi(GTCCR, COM1B1);
+				sbi(GTCCR, PWM1B);
+				OCR1B = val; // set pwm duty
+				break;
+
+
+			case NOT_ON_TIMER:
+			default:
+				if (val < 128) {
+					digitalWrite(pin, LOW);
+				} else {
+					digitalWrite(pin, HIGH);
+				}
+		}
+	}
+}
+
