9b09c09bd23d5b61aa42ad5a348aa2df0f92e85e
Michael Egger <me@anyma.ch>
2013-07-31 16:39:36 +0200

    added instal script for mac

---
+++ patch/mac/install.sh
@@ -0,0 +1,11 @@
+echo "Looking for Arduino Application"
+ARDU="`find "/Applications/" -name "Arduino*.app"`"
+echo "Patching: "$ARDU
+#Contents/Resources/Java/hardware/tools/avr/bin/
+BINPATH=$ARDU/Contents/Resources/Java/hardware/tools/avr/bin/
+ETCPATH=$ARDU/Contents/Resources/Java/hardware/tools/avr/etc/
+mv $BINPATH"avrdude" $BINPATH"avrdude_bak"
+mv $ETCPATH"avrdude.conf" $ETCPATH"avrdude.conf_bak"
+cp avrdude $BINPATH
+cp avrdude.conf $ETCPATH
+echo "Done"
