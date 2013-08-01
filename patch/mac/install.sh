b9a772b4e7641a77316ac667cc311a641f76c7df
Michael Egger <me@anyma.ch>
2013-08-01 20:30:35 +0200

    added both eeded dylibs

--- patch/mac/install.sh
+++ patch/mac/install.sh
@@ -1,4 +1,4 @@
-echo "Looking for Arduino Application"
+	echo "Looking for Arduino Application"
 ARDU="`find "/Applications/" -name "Arduino*.app"`"
 echo "Patching: "$ARDU
 #Contents/Resources/Java/hardware/tools/avr/bin/
@@ -8,5 +8,7 @@ mv $BINPATH"avrdude" $BINPATH"avrdude_bak"
 mv $ETCPATH"avrdude.conf" $ETCPATH"avrdude.conf_bak"
 cp avrdude $BINPATH
 cp avrdude.conf $ETCPATH
-cp libusb /usr/local/lib/libusb-0.1.4.dylib
+mkdir /usr/local/lib/
+cp libusb-0.1.4.dylib /usr/local/lib/libusb-0.1.4.dylib
+cp libusb-1.0.0.dylib /usr/local/lib/libusb-1.0.0.dylib
 echo "Done"
