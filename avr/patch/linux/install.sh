49990e97e17dcb103b9876cf7fbbc345b8373794
stahl <office@stahlnow.com>
2015-04-22 22:22:25 +0200

    removed hacky install script

--- avr/patch/linux/install.sh
+++
@@ -1,6 +0,0 @@
-SP=~/sketchbook/hardware/babygnusbuino/patch/linux/
-PP=/usr/share/arduino/hardware/tools/
-mv $PP"avrdude" $PP"avrdude_bak"
-mv $PP"avrdude.conf" $PP"avrdude.conf_bak"
-cp $SP"avrdude" $PP
-cp $SP"avrdude.conf" $PP
